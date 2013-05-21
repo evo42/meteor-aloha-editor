339a0a5d29bacd1e39e12660fc34195a043fca96
evo42 <rene@evo42.net>
2013-05-21 15:22:21 +0200

    updated Aloha Editor to 0.23.8

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
   //api.use('jquery');
   api.add_files('lib/require.js', 'client');
-  api.add_files('lib/vendor/jquery-1.7.1.js', 'client');
+  api.add_files('lib/vendor/jquery-1.7.2.js', 'client');
   api.add_files('css/aloha.css', 'client');
   api.add_files('aloha-editor-config.js', 'client');
   api.add_files('lib/aloha.js', 'client');
