31c1118bc3be30dc9035ddf68f702f1f376e151c
evo42 <rene@evo42.net>
2013-06-13 20:30:09 +0200

    try to fix not working package

--- package.js
+++ package.js
@@ -5,9 +5,9 @@ Package.describe({
 Package.on_use(function (api) {
   // @todo check why loading is not working anymore
   // -- all files need to be added to the html file manually
-  //api.add_files('lib/require.js', 'client');
-  //api.add_files('lib/vendor/jquery-1.8.2.js', 'client');
-  //api.add_files('aloha-editor-config.js', 'client');
-  //api.add_files('lib/aloha.js', 'client');
+  api.add_files('lib/require.js', 'client');
+  api.add_files('lib/vendor/jquery-1.8.2.js', 'client');
+  api.add_files('aloha-editor-config.js', 'client');
+  api.add_files('lib/aloha.js', 'client');
   api.add_files('css/aloha.css', 'client');
 });
